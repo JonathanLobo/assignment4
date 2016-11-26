07cce7eb895c8f66277eab753dd69d14dc36aa26
Jonathan Lobo <jpl68@pitt.edu>
2016-11-25 22:54:50 -0500

    finished all functionality including clickable links

--- assignment4.js
+++ assignment4.js
@@ -1,17 +1,43 @@
-// The anonymous function below will fire on page load
-
-// Some things to consider
-// $.ajax(); to make your requests a little easier. Or the vanilla js way, it's up to you.
-// $.on(); for event handling
-// Remember, selecting elements in jQuery is like selecting them in CSS
-// You'll probably have to manipulate some strings
-// some jQuery functions to help display results
-// $.show(), $.hide(), $.slideup(), $.slidedown(), $.fadein(), $.fadeout()
-// Add content from requests with something like
-// $.html(), $.text(), etc.
-// keyup events could be helpful to get value of field as the user types
-
 (function() {
-  // Magic!
-  console.log('Keepin\'n it clean with an external script!');
+
+  $('.flexsearch-input').keyup(function(event){
+    var text = $(".flexsearch-input").val();
+    $(".suggestions").html("");
+    getJSON(text);
+  });
+
+  $(document).on('click', '.flexsearch-submit', function(){
+    var text = $(".flexsearch-input").val();
+    if(text.length > 0) {
+      var url = "http://www.google.com/search?q=" + text;
+      window.open(url);
+    }
+  });
+
+  function getJSON(search) {
+    $.ajax({
+      url:"http://www.mattbowytz.com/simple_api.json?data=all",
+      type:"GET",
+      dataType:"json"
+    })
+
+    .done(function(json){
+      json.data.programming.forEach(function(searchText) {
+        searchText = searchText.toLowerCase();
+        if(search.length > 0 && searchText.startsWith(search.toLowerCase())){
+          $(".suggestions").append("<a target=\"_blank\" href=\"http://www.google.com/search?q=" + searchText + "\">" + searchText + "</a>");
+        }
+      });
+      json.data.interests.forEach(function(searchText) {
+        searchText = searchText.toLowerCase();
+        if(search.length > 0 && searchText.startsWith(search.toLowerCase())){
+          $(".suggestions").append("<a target=\"_blank\" href=\"http://www.google.com/search?q=" + searchText + "\">" + searchText + "</a>");
+        }
+      });
+    })
+    
+    .fail(function(){
+      console.log("ERROR!");
+    });
+  }
 })();
